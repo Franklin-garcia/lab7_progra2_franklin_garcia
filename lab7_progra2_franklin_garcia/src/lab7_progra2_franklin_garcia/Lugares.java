37f1c28e49c321c57bf01a003742013af9edec5a
franklin garcia <franklin__garcia@unitec.edu>
2017-09-08 13:28:06 -0600

    trabajndo en usuario

---
+++ lab7_progra2_franklin_garcia/src/lab7_progra2_franklin_garcia/Lugares.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7_progra2_franklin_garcia;
+
+/**
+ *
+ * @author Franklin Garcia
+ */
+public class Lugares {
+   private String nombre;
+   private String clima;
+   private String extensión;
+   private String territorial;
+   private String habitantes; 
+   private String zona;
+   private String fundacion;
+
+    public Lugares() {
+    }
+
+    public Lugares(String nombre, String clima, String extensión, String territorial, String habitantes, String zona, String fundacion) {
+        this.nombre = nombre;
+        this.clima = clima;
+        this.extensión = extensión;
+        this.territorial = territorial;
+        this.habitantes = habitantes;
+        this.zona = zona;
+        this.fundacion = fundacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClima() {
+        return clima;
+    }
+
+    public void setClima(String clima) {
+        this.clima = clima;
+    }
+
+    public String getExtensión() {
+        return extensión;
+    }
+
+    public void setExtensión(String extensión) {
+        this.extensión = extensión;
+    }
+
+    public String getTerritorial() {
+        return territorial;
+    }
+
+    public void setTerritorial(String territorial) {
+        this.territorial = territorial;
+    }
+
+    public String getHabitantes() {
+        return habitantes;
+    }
+
+    public void setHabitantes(String habitantes) {
+        this.habitantes = habitantes;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public String getFundacion() {
+        return fundacion;
+    }
+
+    public void setFundacion(String fundacion) {
+        this.fundacion = fundacion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+   
+}
--- lab7_progra2_franklin_garcia/src/lab7_progra2_franklin_garcia/Personas.java
+++ lab7_progra2_franklin_garcia/src/lab7_progra2_franklin_garcia/Personas.java
@@ -10,5 +10,76 @@ package lab7_progra2_franklin_garcia;
  * @author Franklin Garcia
  */
 public class Personas {
-    
+  private String nombre;
+  private String id;
+  private String lugar; 
+  private String edad;
+  private String estatura;
+  private String profesión;
+
+    public Personas() {
+    }
+
+    public Personas(String nombre, String id, String lugar, String edad, String estatura, String profesión) {
+        this.nombre = nombre;
+        this.id = id;
+        this.lugar = lugar;
+        this.edad = edad;
+        this.estatura = estatura;
+        this.profesión = profesión;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(String estatura) {
+        this.estatura = estatura;
+    }
+
+    public String getProfesión() {
+        return profesión;
+    }
+
+    public void setProfesión(String profesión) {
+        this.profesión = profesión;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+  
 }
